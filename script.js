b9a19319ef10b4644e3bb06e915d815c1c667011
DinguzTheDev <128646877+DinguzTheDev@users.noreply.github.com>
2026-01-16 15:01:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+window.addEventListener('load', () => {
+    // Dodaj klasę visible do body, co uruchomi transition opacity w CSS
+    document.body.classList.add('visible');
+    
+    console.log("Dinguz Launcher site loaded.");
+});
